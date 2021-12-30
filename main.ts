ad9bea4e6be6b25e2384aaf2408f2d8549fa4289
snowneon <snowneon@gmail.com>
2021-12-29 20:34:31 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,27 @@
-basic.showIcon(IconNames.Heart)
-basic.showLeds(`
-    . # . # .
-    # . # . #
-    . # . # .
-    # . # . #
-    . # . # .
-    `)
 SuperBit.MotorRun(SuperBit.enMotors.M3, 255)
+basic.forever(function () {
+    basic.showString("I love dad")
+    basic.showIcon(IconNames.Tortoise)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        . # . # .
+        # . # . #
+        . # . # .
+        `)
+    basic.showLeds(`
+        # . # . #
+        # . # . #
+        # . # . #
+        # . # . #
+        # . # . #
+        `)
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . # . # .
+        . # . # .
+        . # . # .
+        `)
+    music.playMelody("C5 B A G F E D C ", 120)
+})
