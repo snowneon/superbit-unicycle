3e5cec9a1151fbbe529b2ea962da4885e8ab37f3
snowneon <snowneon@gmail.com>
2021-12-30 20:11:29 -0600

    controlled led on expansion board

--- main.ts
+++ main.ts
@@ -1,6 +1,11 @@
-SuperBit.MotorRun(SuperBit.enMotors.M3, 255)
+SuperBit.RGB_Program().setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+SuperBit.RGB_Program().setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+SuperBit.RGB_Program().setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+SuperBit.RGB_Program().setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+SuperBit.RGB_Program().show()
+SuperBit.MotorRun(SuperBit.enMotors.M2, 255)
 basic.forever(function () {
-    basic.showString("I love dad and mom")
+    basic.showString("I love dad, mom and Ada")
     basic.showIcon(IconNames.Tortoise)
     basic.showLeds(`
         . # . # .
